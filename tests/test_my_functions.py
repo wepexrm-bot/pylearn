6fb8039936d2822791f07e860d0c43e51527f5ee
wepex <wepexrm@gmail.com>
2026-08-22 13:35:10 +0530

    Class based test

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -5,10 +5,14 @@ def test_add():
     result  = my_functions.add( num1 = 5 , num2 = 6)
     assert result == 11
 
+def test_add_strings():
+    result = my_functions.add(num1 ="two" , num2= "one")
+    assert result == "twoone"
+
 def test_divide():
     result = my_functions.divide(num1 = 10, num2 = 5)
     assert result == 2
 
 def test_division_by_zero():
-    with pytest.raises(ZeroDivisionError):
+    with pytest.raises(ValueError):
         my_functions.divide(num1 = 10, num2 = 0)
