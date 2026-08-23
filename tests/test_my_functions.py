47c615e3d2700cdc0e2d923f4cefc10026652509
wepex <wepexrm@gmail.com>
2026-08-23 09:53:53 +0530

    Database testing

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 import source.my_functions as my_functions
 
 def test_add():
@@ -16,3 +17,19 @@ def test_divide():
 def test_division_by_zero():
     with pytest.raises(ValueError):
         my_functions.divide(num1 = 10, num2 = 0)
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(num1 = 10, num2 = 5)
+    assert result == 2
+
+
+@pytest.mark.skip(reason = "This feature is currently broken")#for skipping tests
+ 
+def test_add():
+    assert my_functions.add(num1= 1 , num2= 2) == 3
+
+@pytest.mark.xfail(reason= "we know we cannot divide by 0" )
+def test_divide_zero_brokken():
+    my_functions.divide(num1= 4, num2= 0)
---
+++ tests/test_service.py
@@ -0,0 +1,21 @@
+import pytest
+import source.service as service
+import unittest.mock as mock
+
+@mock.patch("source.service.get_user_from_db")
+def test_get_user_from_db(mock_get_user_from_db):
+    mock_get_user_from_db.return_value = "Mocked Alice"
+    user_name = service.get_user_from_db(1)
+
+    assert user_name == "Mocked Alice"
+
+
+@mock.patch("requests.get")
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = { "id": 1, "name": "John Doe"}
+    mock_get.return_value = mock_response
+    data = service.get_users()
+    assert data == {"id": 1, "name": "John Doe"}
+    
