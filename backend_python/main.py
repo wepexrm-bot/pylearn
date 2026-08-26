377036f19058bc02ad4706311c38a4cdbd72b1be
wepex <wepexrm@gmail.com>
2026-08-26 23:09:24 +0530

    Add posts CRUD: list, create, get by ID

--- backend_python/main.py
+++ backend_python/main.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -12,19 +14,33 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, 
+            {"title": "favourite foods", "content": "i like pizza" , "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")  #path operation
 def root(): #function
     return {"message" : "Welcome to my api !!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data" : "This is your posts"}
+    return {"data" : my_posts}
 
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.model_dump())
-    return {"data": "post"}
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0 ,10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
-#title str, content str, category, bool published
+
+@app.get("/posts/{id}")
+def get_post(id):
+    post = find_post(id)
+    return {"post_detail": post}
+
